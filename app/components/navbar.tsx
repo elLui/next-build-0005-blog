5ba705ab4457020e429b2a9bab7c02cdaef03dcc
elluis <elluis.codes@gmail.com>
2023-05-08 04:39:07 -0500

    psuedo navbar.tsx implemented and rendered in RootLayout

---
+++ app/components/navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+
+    return (
+        <nav className="bg-gradient-to-tr from-amber-500 to-sky-400 p-4 sticky top-0 drop-shadow-xl z-10">
+            navbar
+        </nav>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import clsx from "clsx";
+import Navbar from "@/app/components/navbar";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={clsx(inter.className, "dark:bg-gradient-radial from-pink-700 to-sky-400")}>
+
+      <Navbar />
+
+      {children}</body>
     </html>
   )
 }
