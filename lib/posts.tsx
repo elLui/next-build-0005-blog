09fcb27066974331679c533bc8751f42f08b4aff
elluis <elluis.codes@gmail.com>
2023-05-10 03:54:25 -0500

    added a new BlogPost type just a save point -
    had several issues with grabbing our API information - after several changes and we finally realized that - I forgot the repo was still marked as private -
    
    after finally changing the repo to public - waaaahla!!
    it works ;-| -
    https://api.github.com/repos/ellui/next-build-0005-blog/git/trees/main?recursive=1
    ===
    also it await! -- not wait

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -1,52 +1,72 @@
+import {compileMDX} from 'next-mdx-remote/rsc';
+
 type Filetree = {
     "tree": [
         {
             "path": string,
-        }
-    ]
+        },
+    ],
 }
 
-export async function getPostByName(fileName: string) Promise<Blogpost | undefined>
-
-
-
-export async function getPostMeta(): Promise<Meta[] | undefined> {
-
-    const res = await fetch('https://api.github.com/repos/ellui/next-build-0005-blog/git/trees/main?recursive=1', {
+export async function getPostByName(fileName: string): Promise<BlogPost | undefined> {
+    const res = await fetch(`https://raw.githubusercontent.com/ellui/next-build-0005-blog/main/${fileName}`, {
         headers: {
             'Accept': 'application/vnd.github+json',
             'Authorization': `Bearer ${process.env.GITHUB_TOKEN}`,
             'X-GitHub-Api-Version': '2022-11-28',
-        }
+        },
     })
 
-    // test the response for errors
     if (!res.ok) {
-        console.error(res.statusText)
         return undefined
     }
 
-    // get raw response body
-    const repoFiletree: Filetree = await res.json();
+    const rawMDX = await res.text();
 
-    // filter the tree for only .mdx files
-    const filesArray = repoFiletree.tree.map(obj => obj.path).filter(path => path.endsWith('.mdx'));
-
-    // create a container for the metadata
-    const posts: Meta[] = [];
-
-    // loop through the files and get the metadata
-    for (const file of filesArray) {
-        const post = await getPostByName(file);
-        if (post) {
-            const {meta} = post;
-            posts.push(meta);
-        }
+    if (rawMDX === '404: Not Found') {
+        return undefined
     }
 
+    const {frontmatter, content} = await compileMDX<{ title: string, date: string, description: string[] }>({
 
-    // sort post by date
-    return posts.sort((a, b) => a.date < b.date ? 1 : -1);
+        source: rawMDX,
+    })
 
 
-}
+    export async function getPostMeta(): Promise<Meta[] | undefined> {
+
+        const res = await fetch('https://api.github.com/repos/ellui/next-build-0005-blog/git/trees/main?recursive=1', {
+            headers: {
+                'Accept': 'application/vnd.github+json',
+                'Authorization': `Bearer ${process.env.GITHUB_TOKEN}`,
+                'X-GitHub-Api-Version': '2022-11-28',
+            }
+        })
+
+        // test the response for errors
+        if (!res.ok) {
+            console.error(res.statusText)
+            return undefined
+        }
+
+        // get raw response body
+        const repoFiletree: Filetree = await res.json();
+
+        // filter the tree for only .mdx files
+        const filesArray = repoFiletree.tree.map(obj => obj.path).filter(path => path.endsWith('.mdx'));
+
+        // create a container for the metadata
+        const posts: Meta[] = [];
+
+        // loop through the files and get the metadata
+        for (const file of filesArray) {
+            const post = await getPostByName(file);
+            if (post) {
+                const {meta} = post;
+                posts.push(meta);
+            }
+        }
+
+        // sort post by date
+        return posts.sort((a, b) => a.date < b.date ? 1 : -1);
+    }
