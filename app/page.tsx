2598b163d9f1012687d914032e7e546ecb644b5e
elluis <elluis.codes@gmail.com>
2023-05-10 00:38:01 -0500

    initial commit -
    we have cloned :: https://github.com/elLui/next-build-0005-blog -
    picking back up on some revalidation ::  https://www.youtube.com/watch?v=Q3UP1Is8Pvw&list=RDCMUCY38RvRIxYODO4penyxUwTg&index=7 -
    
    notes ::
    not implementing any restAPI interface as they should not be needed for this project - yet -

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,20 @@
 import Posts from "@/app/components/Posts";
 
+
+export const revalidate: number = 10;
 export default function Home() {
-  return (
-    <main className="px-6 mx-auto">
-      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
-        que onda and welcome  🏃‍♂️&nbsp;
-          <span className="">
+    return (
+
+        <main className="px-6 mx-auto">
+            <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+                que onda and welcome 🏃‍♂️&nbsp;
+                <span className="">
               this &apos; is a span, <span className="text-red-500">there are many like it</span>, but this one is mine.
           </span>
-      </p>
-        <Posts />
+            </p>
 
+            <Posts/>
 
-
-
-    </main>
-  )
+        </main>
+    )
 }
--- blogposts/new.md
+++ blogposts/new.md
@@ -1,6 +1,6 @@
 ---
-title: 'Two Forms of Pre-rendering'
-date: '2023-03-14'
+title: 'Another New Post'
+date: '2023-03-15'
 ---
 
 Next.js has two forms of pre-rendering: **Static Generation** and **Server-side Rendering**. The difference is in **when** it generates the HTML for a page.
